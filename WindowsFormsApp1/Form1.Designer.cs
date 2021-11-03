a1bdbda0674037ba8627e60e70b482a790b495fe
Kajetan Krasoń <kajetan@czaplinek.pl>
2021-11-03 02:28:51 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonStopRecord = new System.Windows.Forms.Button();
+            this.buttonStartAfterPause = new System.Windows.Forms.Button();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxRecordingDevice = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxPlaybackDevice = new System.Windows.Forms.ComboBox();
+            this.buttonRecordMic = new System.Windows.Forms.Button();
+            this.buttonStopRecordMic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(466, 65);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(334, 272);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(24, 115);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(89, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(160, 115);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(95, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(24, 162);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(89, 23);
+            this.buttonPause.TabIndex = 5;
+            this.buttonPause.Text = "PAUSE";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PlaySound",
+            "WMP",
+            "WaveOutWrite",
+            "MCI",
+            "DirectSound"});
+            this.comboBox1.Location = new System.Drawing.Point(288, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(24, 212);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(89, 34);
+            this.buttonRecord.TabIndex = 7;
+            this.buttonRecord.Text = "RECORD";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click_1);
+            // 
+            // buttonStopRecord
+            // 
+            this.buttonStopRecord.Location = new System.Drawing.Point(160, 211);
+            this.buttonStopRecord.Name = "buttonStopRecord";
+            this.buttonStopRecord.Size = new System.Drawing.Size(95, 35);
+            this.buttonStopRecord.TabIndex = 8;
+            this.buttonStopRecord.Text = "STOP RECORD";
+            this.buttonStopRecord.UseVisualStyleBackColor = true;
+            this.buttonStopRecord.Click += new System.EventHandler(this.buttonStopRecord_Click_1);
+            // 
+            // buttonStartAfterPause
+            // 
+            this.buttonStartAfterPause.Location = new System.Drawing.Point(160, 162);
+            this.buttonStartAfterPause.Name = "buttonStartAfterPause";
+            this.buttonStartAfterPause.Size = new System.Drawing.Size(95, 43);
+            this.buttonStartAfterPause.TabIndex = 9;
+            this.buttonStartAfterPause.Text = "START AFTER PAUSE";
+            this.buttonStartAfterPause.UseVisualStyleBackColor = true;
+            this.buttonStartAfterPause.Click += new System.EventHandler(this.buttonStartAfterPause_Click_1);
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Location = new System.Drawing.Point(24, 65);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(231, 21);
+            this.buttonChooseFile.TabIndex = 10;
+            this.buttonChooseFile.Text = "Choose File";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Recording Device";
+            // 
+            // comboBoxRecordingDevice
+            // 
+            this.comboBoxRecordingDevice.FormattingEnabled = true;
+            this.comboBoxRecordingDevice.Location = new System.Drawing.Point(120, 276);
+            this.comboBoxRecordingDevice.Name = "comboBoxRecordingDevice";
+            this.comboBoxRecordingDevice.Size = new System.Drawing.Size(244, 21);
+            this.comboBoxRecordingDevice.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Playback Device";
+            // 
+            // comboBoxPlaybackDevice
+            // 
+            this.comboBoxPlaybackDevice.FormattingEnabled = true;
+            this.comboBoxPlaybackDevice.Location = new System.Drawing.Point(120, 331);
+            this.comboBoxPlaybackDevice.Name = "comboBoxPlaybackDevice";
+            this.comboBoxPlaybackDevice.Size = new System.Drawing.Size(244, 21);
+            this.comboBoxPlaybackDevice.TabIndex = 14;
+            // 
+            // buttonRecordMic
+            // 
+            this.buttonRecordMic.Location = new System.Drawing.Point(24, 376);
+            this.buttonRecordMic.Name = "buttonRecordMic";
+            this.buttonRecordMic.Size = new System.Drawing.Size(85, 41);
+            this.buttonRecordMic.TabIndex = 15;
+            this.buttonRecordMic.Text = "RECORD MIC";
+            this.buttonRecordMic.UseVisualStyleBackColor = true;
+            this.buttonRecordMic.Click += new System.EventHandler(this.buttonRecordMic_Click);
+            // 
+            // buttonStopRecordMic
+            // 
+            this.buttonStopRecordMic.Location = new System.Drawing.Point(160, 376);
+            this.buttonStopRecordMic.Name = "buttonStopRecordMic";
+            this.buttonStopRecordMic.Size = new System.Drawing.Size(95, 41);
+            this.buttonStopRecordMic.TabIndex = 16;
+            this.buttonStopRecordMic.Text = "STOP RECORD MIC";
+            this.buttonStopRecordMic.UseVisualStyleBackColor = true;
+            this.buttonStopRecordMic.Click += new System.EventHandler(this.buttonStopRecordMic_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(812, 429);
+            this.Controls.Add(this.buttonStopRecordMic);
+            this.Controls.Add(this.buttonRecordMic);
+            this.Controls.Add(this.comboBoxPlaybackDevice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxRecordingDevice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonChooseFile);
+            this.Controls.Add(this.buttonStartAfterPause);
+            this.Controls.Add(this.buttonStopRecord);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonStopRecord;
+        private System.Windows.Forms.Button buttonStartAfterPause;
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxRecordingDevice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxPlaybackDevice;
+        private System.Windows.Forms.Button buttonRecordMic;
+        private System.Windows.Forms.Button buttonStopRecordMic;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using NAudio.Wave;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        WaveIn waveRecord;
+        WaveFileWriter writerRecord;
+        string PlayCommand;
+        bool isOpen;
+        string output_file_name;
+        string file_name;
+        WasapiLoopbackCapture capture;
+
+        NAudio.Wave.WaveFileReader wave = null;
+        NAudio.Wave.DirectSoundOut output = null;
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr oCallback);
+
+        //[DllImport("winmm.dll")]
+        //public static extern int waveOutOpen(out IntPtr hWaveOut, int uDeviceID, Microsoft.DirectX.DirectSound.WaveFormat lpFormat, WaveDelegate dwCallback, IntPtr dwInstance, int dwFlags);
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDevices();
+            buttonStopRecord.Enabled = false;
+            buttonStopRecordMic.Enabled = false;
+        }
+
+        private void LoadDevices()
+        {
+            for(int deviceId = 0; deviceId < WaveIn.DeviceCount; deviceId++)
+            {
+                var deviceInfo = WaveIn.GetCapabilities(deviceId);
+                comboBoxRecordingDevice.Items.Add(deviceInfo.ProductName);
+            }
+
+            for (int deviceId = 0; deviceId < WaveIn.DeviceCount; deviceId++)
+            {
+                var deviceInfo = WaveIn.GetCapabilities(deviceId);
+                comboBoxPlaybackDevice.Items.Add(deviceInfo.ProductName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "PlaySound")
+            {
+                System.Media.SoundPlayer SoundCardHandler = new System.Media.SoundPlayer(file_name);
+                SoundCardHandler.Play();
+            }
+            if ((string)comboBox1.SelectedItem == "WMP")
+            {
+                axWindowsMediaPlayer1.URL = file_name;
+            }
+            if ((string)comboBox1.SelectedItem == "WaveOutWrite")
+            {
+
+            }
+            if ((string)comboBox1.SelectedItem == "MCI")
+            {
+                PlayCommand = "Open \"" + file_name + "\" alias MediaFile";
+                mciSendString(PlayCommand, null, 0, IntPtr.Zero);
+                isOpen = true;
+
+                if (isOpen)
+                {
+                    PlayCommand = "Play MediaFile";
+                    mciSendString(PlayCommand, null, 0, IntPtr.Zero);
+                }
+            }
+            if ((string)comboBox1.SelectedItem == "DirectSound")
+            {
+                wave = new NAudio.Wave.WaveFileReader(file_name);
+                output = new NAudio.Wave.DirectSoundOut();
+                output.Init(new NAudio.Wave.WaveChannel32(wave));
+
+                output.Play();
+            }
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "PlaySound")
+            {
+                System.Media.SoundPlayer SoundCardHandler = new System.Media.SoundPlayer();
+                SoundCardHandler.Stop();
+            }
+            if ((string)comboBox1.SelectedItem == "WMP")
+            {
+                axWindowsMediaPlayer1.URL = "";
+            }
+            if ((string)comboBox1.SelectedItem == "WaveOutWrite")
+            {
+
+            }
+            if ((string)comboBox1.SelectedItem == "MCI")
+            {
+                PlayCommand = "Close MediaFile";
+                mciSendString(PlayCommand, null, 0, IntPtr.Zero);
+                isOpen = false;
+            }
+            if ((string)comboBox1.SelectedItem == "DirectSound")
+            {
+                output.Stop();
+            }
+        }
+
+        private void buttonChooseFile_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = @"Wave File (*.wav)|*.wav";
+            if (fileDialog.ShowDialog() != DialogResult)
+            {
+                file_name = fileDialog.InitialDirectory + fileDialog.FileName;
+            }
+        }
+
+        private void buttonRecord_Click_1(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "Wave files | *.wav";
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            output_file_name = dialog.FileName;
+
+            buttonRecord.Enabled = false;
+            buttonStopRecord.Enabled = true;
+
+            capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(output_file_name, capture.WaveFormat);
+
+            capture.DataAvailable += async (s, c) =>
+            {
+                if (writer != null)
+                {
+                    await writer.WriteAsync(c.Buffer, 0, c.BytesRecorded);
+                    await writer.FlushAsync();
+                }
+            };
+
+            capture.RecordingStopped += (s, c) =>
+            {
+                if (writer != null)
+                {
+                    writer.Dispose();
+                    writer = null;
+                }
+
+                buttonRecord.Enabled = true;
+                capture.Dispose();
+            };
+
+            capture.StartRecording();
+        }
+
+        
+
+        private void buttonStopRecord_Click_1(object sender, EventArgs e)
+        {
+            buttonStopRecord.Enabled = false;
+            capture.StopRecording();
+
+            if (output_file_name == null)
+                return;
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = Path.GetDirectoryName(output_file_name),
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        }
+
+        private void buttonStartAfterPause_Click_1(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "WMP")
+            {
+                WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+                controls.play();
+            }
+            if ((string)comboBox1.SelectedItem == "MCI")
+            {
+                string mciResume = "resume MediaFile";
+                mciSendString(mciResume, null, 0, IntPtr.Zero);
+            }
+            if ((string)comboBox1.SelectedItem == "DirectSound")
+            {
+                output.Play();
+            }
+        }
+
+        private void buttonPause_Click_1(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "PlaySound")
+            {
+                MessageBox.Show("Nie da sie zapauzowac!");
+            }
+            if ((string)comboBox1.SelectedItem == "WMP")
+            {
+                WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+                controls.pause();
+            }
+            if ((string)comboBox1.SelectedItem == "WaveOutWrite")
+            {
+
+            }
+            if ((string)comboBox1.SelectedItem == "MCI")
+            {
+                string mciPause = "pause Mediafile";
+                mciSendString(mciPause, null, 0, IntPtr.Zero);
+            }
+            if ((string)comboBox1.SelectedItem == "DirectSound")
+            {
+                output.Pause();
+            }
+        }
+
+        private void buttonRecordMic_Click(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "Wave files | *.wav";
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            output_file_name = dialog.FileName;
+
+            buttonRecordMic.Enabled = false;
+            buttonStopRecordMic.Enabled = true;
+
+            waveRecord = new WaveIn();
+            waveRecord.WaveFormat = new WaveFormat(44100, 1);
+            waveRecord.DeviceNumber = comboBoxRecordingDevice.SelectedIndex;
+            waveRecord.DataAvailable += Wave_DataAvailable;
+            waveRecord.RecordingStopped += Wave_RecordingStopped;
+            writerRecord = new WaveFileWriter(output_file_name, waveRecord.WaveFormat);
+            waveRecord.StartRecording();
+        }
+
+        private void buttonStopRecordMic_Click(object sender, EventArgs e)
+        {
+            buttonStopRecordMic.Enabled = false;
+            waveRecord.StopRecording();
+
+            if (output_file_name == null)
+                return;
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = Path.GetDirectoryName(output_file_name),
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        }
+
+        private void Wave_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            writerRecord.Dispose();
+            buttonRecordMic.Enabled = true;
+            buttonStopRecordMic.Enabled = false;
+        }
+
+        private void Wave_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            writerRecord.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+    }
+}
